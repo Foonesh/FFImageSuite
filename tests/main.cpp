16a9805d52d5a3f7ae4c792e249069ea5a73ccb1
Daniel Koperniak <dannkop@gmail.com>
2017-02-02 16:58:01 +0100

    tests added

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include <commands/command_factory.hpp>
 
-TEST(ff, exitWhenExitCommandPassed)
+TEST(basicTests, whenHelpPassedThenResIsNotNullptr)
 {
+    auto res = command_factory::construct("help");
+    EXPECT_NE(res, nullptr);
+}
+
+TEST(basicTests, whenExitPassedThenResIsNotNullptr)
+{
+    auto res = command_factory::construct("exit");
+    EXPECT_NE(res, nullptr);
+}
+
+TEST(basicTests, whenRotatePassedThenResIsNotNullptr)
+{
+    auto res = command_factory::construct("rotate");
+    EXPECT_NE(res, nullptr);
+}
+
+TEST(basicTests, whenSplitPassedThenResIsNotNullptr)
+{
+    auto res = command_factory::construct("split");
+    EXPECT_NE(res, nullptr);
+}
+
+TEST(basicTests, whenGreyScalePassedThenResIsNotNullptr)
+{
+    auto res = command_factory::construct("greyscale");
+    EXPECT_NE(res, nullptr);
+}
+
+TEST(basicTests, whenLoadBmpPassedThenResIsNotNullptr)
+{
+    auto res = command_factory::construct("load_bmp");
+    EXPECT_NE(res, nullptr);
 }
 
 int main(int argc, char **argv)
